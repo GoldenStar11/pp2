8f49755e54c5536b7023a68fad9803949df6fd4c
Yerkebulan <bekmukhayev@gmail.com>
2019-02-14 18:12:13 +0600

    LAB 1 reload

--- LAB 1/Task 2/Program.cs
+++ LAB 1/Task 2/Program.cs
@@ -2,29 +2,29 @@
 
 namespace Task_2
 {
-	class Student
-	{
-		public string name;
-		public string id;
-		public string year;
+    class Student
+    {
+        public string name;
+        public string id;
+        public string year;
 
-		public void PrintInfo()
-		{
-			Console.WriteLine(name + " " + id + " " + year);
-		}
-	}
+        public void PrintInfo()
+        {
+            Console.WriteLine(name + " " + id + " " + year);
+        }
+    }
 
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Student s = new Student
-			{
-				name = Console.ReadLine(),
-				id = Console.ReadLine(),
-				year = Console.ReadLine()
-			};
-			s.PrintInfo();
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student
+            {
+                name = Console.ReadLine(),
+                id = Console.ReadLine(),
+                year = Console.ReadLine()
+            };
+            s.PrintInfo();
+        }
+    }
 }
