2d957dc235005508bcc54c7fcd7177b97ca6a4f7
Yerkebulan <bekmukhayev@gmail.com>
2019-02-14 16:47:33 +0600

    LAB 1

---
+++ LAB 1/Task 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_2
+{
+	class Student
+	{
+		public string name;
+		public string id;
+		public string year;
+
+		public void PrintInfo()
+		{
+			Console.WriteLine(name + " " + id + " " + year);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Student s = new Student
+			{
+				name = Console.ReadLine(),
+				id = Console.ReadLine(),
+				year = Console.ReadLine()
+			};
+			s.PrintInfo();
+		}
+	}
+}
